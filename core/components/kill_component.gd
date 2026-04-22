540bb201966175da85b40e1ad7cabd0eff1c487a
Pilen <fiskomaten@gmail.com>
2026-04-22 19:11:41 +0200

    Add KillComponent

---
+++ core/components/kill_component.gd
@@ -0,0 +1,9 @@
+@icon("res://assets/icons/configurable.svg")
+class_name KillComponent
+extends Node
+
+func _ready() -> void:
+	set_meta("is_action", self)
+
+func action(who: Node2d) -> bool:
+	owner.queue_free()
